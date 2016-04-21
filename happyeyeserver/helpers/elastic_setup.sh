01d679ef1954b0ac91c20a6d483ba1a60f576764
Lars Kåre Skjørestad <lars.kaare@skjorestad.com>
2016-04-21 13:26:11 +0200

    Put a comment on the index delte part ...

--- happyeyeserver/helpers/elastic_setup.sh
+++ happyeyeserver/helpers/elastic_setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -XDELETE 'http://localhost:9200/happymeeter' 
+#curl -XDELETE 'http://localhost:9200/happymeeter' 
 curl -XPOST localhost:9200/happymeeter -d '{
     "settings" : {
         "number_of_shards" : 1
